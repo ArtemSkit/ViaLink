3b30e9a67c27da21bafce8e754f3fed927ed1176
Joshua Madrigal <madrigaljosh10@yahoo.com>
2019-02-23 16:26:01 -0600

    ARRIVAL LIST

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>ViaLink</h1>
+    <p> Application that will tell a user when a bus is arriving, 
+      while using geolocation on their phone to notify transit </p>
   </div>
 </template>
